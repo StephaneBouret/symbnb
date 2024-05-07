ff4097b16abbf3db1195c19219f709245fb8bb3c
Stéphane BOURET <sbouret@orange.fr>
2024-05-07 17:13:19 +0200

    Mise en place des entités User et Ad et du bundle Security

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,70 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Ad;
+use Faker\Factory;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $slugger;
+    protected $passwordHasher;
+
+    public function __construct(SluggerInterface $slugger, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->slugger = $slugger;
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $admin = new User;
+
+        $hash = $this->passwordHasher->hashPassword($admin, "password");
+        $admin->setFirstname('admin')
+            ->setLastname('admin')
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN'])
+            ->setAdress($faker->streetAddress)
+            ->setPostalCode($faker->postcode)
+            ->setCity($faker->city)
+            ->setEmail("admin@gmail.com")
+            ->setPhone($faker->mobileNumber());
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+            $hash = $this->passwordHasher->hashPassword($user, "password");
+            $user->setEmail("user$u@gmail.com")
+                ->setFirstname($faker->firstName())
+                ->setLastname($faker->lastName())
+                ->setPassword($hash)
+                ->setAdress($faker->streetAddress)
+                ->setPostalCode($faker->postcode)
+                ->setCity($faker->city)
+                ->setPhone($faker->mobileNumber());
+
+            $manager->persist($user);
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $ad = new Ad;
+            $ad->setName($faker->sentence())
+                ->setSlug($this->slugger->slug($ad->getName()))
+                ->setIntroduction($faker->paragraph(2))
+                ->setContent($faker->paragraph())
+                ->setRooms($faker->numberBetween(1, 6))
+                ->setCapacity($faker->numberBetween(2, 10))
+                ->setPrice($faker->numberBetween(25000, 100000));
+
+            $manager->persist($ad);
+        }
 
         $manager->flush();
     }
---
+++ src/Entity/Ad.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdRepository::class)]
+class Ad
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Column]
+    private ?int $capacity = null;
+
+    #[ORM\Column]
+    private ?int $rooms = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $introduction = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(options: ['default' => 'CURRENT_TIMESTAMP'])]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): static
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getRooms(): ?int
+    {
+        return $this->rooms;
+    }
+
+    public function setRooms(int $rooms): static
+    {
+        $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    public function getIntroduction(): ?string
+    {
+        return $this->introduction;
+    }
+
+    public function setIntroduction(string $introduction): static
+    {
+        $this->introduction = $introduction;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
